02420ce4a2bc01ef384f320670d8a1ee810b0744
KJTempas <fw6927mm@go.minneapolis.edu>
2021-02-11 10:10:37 -0600

    fix bug in delete

--- puzzle_share/urls.py
+++ puzzle_share/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('available', views.puzzles_available, name='puzzles_available'),
     path('add_puzzle', views.add_puzzle, name='add_puzzle'),
     path('puzzle/<int:puzzle_pk>', views.puzzle_details, name='puzzle_details'),
-    path('puzzle/<int:puzzle_pk>', views.delete_puzzle, name='delete_puzzle')
+    path('puzzle/<int:puzzle_pk>/delete', views.delete_puzzle, name='delete_puzzle')
 ]
